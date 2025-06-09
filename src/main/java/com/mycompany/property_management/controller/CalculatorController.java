495747c0e010a569cdb1ba517efb10251dd1e31e
Nishaldevadiga <65213089+Nishaldevadiga@users.noreply.github.com>
2025-06-09 18:44:58 -0400

    "statuc code"

--- src/main/java/com/mycompany/property_management/controller/CalculatorController.java
+++ src/main/java/com/mycompany/property_management/controller/CalculatorController.java
@@ -1,5 +1,8 @@
 package com.mycompany.property_management.controller;
 
+import com.mycompany.property_management.dto.CalculatorDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 //http://localhost:8080/api/vi/calculator/add/4?num1=3&num2=4
@@ -11,10 +14,22 @@ public class CalculatorController {
             ("num2")Double num2,@PathVariable("num3") Double num3){
        return num1+num2+num3;
     }
+
+
     //http://localhost:8080/api/vi/calculator/sub/6.0/3.0
     @GetMapping("/sub/{num1}/{num2}")
     public Double sub(@PathVariable("num1") Double num1, @PathVariable
             ("num2")Double num2){
         return num1-num2;
     }
+
+    //http://localhost:8080/api/vi/calculator/mul
+    @PostMapping("/mul")
+    public ResponseEntity<Double> multiply(@RequestBody CalculatorDTO calculatorDTO){
+Double result=null;
+result=calculatorDTO.getNum1()*calculatorDTO.getNum2()*calculatorDTO.getNum3()*calculatorDTO.getNum4();
+        ResponseEntity<Double> res=new ResponseEntity<Double>(result, HttpStatus.CREATED);
+return res;
+    }
+
 }
