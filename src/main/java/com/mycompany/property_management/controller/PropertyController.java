b2aef547c87a1ff7ea5d9638b5e61b8343ca97cb
Nishaldevadiga <65213089+Nishaldevadiga@users.noreply.github.com>
2025-06-10 15:09:06 -0400

    "statuc code"

--- src/main/java/com/mycompany/property_management/controller/PropertyController.java
+++ src/main/java/com/mycompany/property_management/controller/PropertyController.java
@@ -1,16 +1,22 @@
 package com.mycompany.property_management.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.mycompany.property_management.dto.PropertyDTO;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("api/v1/properties")
+@RequestMapping("/api/v1")
 public class PropertyController {
 
 
     @GetMapping("/hello")
     public String sayHello(){
         return "hello";
+    }
+
+//localhost:8080/api/v1/saveproperty
+    @PostMapping("/saveproperty")
+    public PropertyDTO saveproperty(@RequestBody PropertyDTO propertydto){
+         System.out.print(propertydto);
+         return propertydto;
     }
 }
