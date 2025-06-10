5c4c3d3a63931714e7c7f904103a0bf539a86364
Nishaldevadiga <65213089+Nishaldevadiga@users.noreply.github.com>
2025-06-10 17:43:00 -0400

    Merge remote-tracking branch 'origin/master'

---
+++ src/main/java/com/mycompany/property_management/service/impl/PropertyServiceimpl.java
@@ -0,0 +1,15 @@
+package com.mycompany.property_management.service.impl;
+
+import com.mycompany.property_management.dto.PropertyDTO;
+import com.mycompany.property_management.service.PropertyService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PropertyServiceimpl implements PropertyService {
+
+    @Override
+
+    public PropertyDTO saveproperty(PropertyDTO propertydto) {
+        return null;
+    }
+}
