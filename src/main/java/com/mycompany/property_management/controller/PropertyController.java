35094582f745be866b9cf0e939f4a89908977eed
Nishaldevadiga <65213089+Nishaldevadiga@users.noreply.github.com>
2025-06-10 17:43:00 -0400

    Merge remote-tracking branch 'origin/master'

--- src/main/java/com/mycompany/property_management/controller/PropertyController.java
+++ src/main/java/com/mycompany/property_management/controller/PropertyController.java
@@ -1,11 +1,17 @@
 package com.mycompany.property_management.controller;
 
 import com.mycompany.property_management.dto.PropertyDTO;
+import com.mycompany.property_management.service.PropertyService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1")
 public class PropertyController {
+
+
+    @Autowired
+    private PropertyService propertyService;
 
 
     @GetMapping("/hello")
@@ -16,6 +22,7 @@ public class PropertyController {
 //localhost:8080/api/v1/saveproperty
     @PostMapping("/saveproperty")
     public PropertyDTO saveproperty(@RequestBody PropertyDTO propertydto){
+        propertyService.saveproperty(propertydto);
          System.out.print(propertydto);
          return propertydto;
     }
